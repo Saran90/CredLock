ed013656d4de990d9ae510278fb49aa0df9f62eb
Saran <saran.mampatta90@gmail.com>
2026-08-03 18:05:10 +0530

    chore: update Android namespace and applicationId to com.yuklore.credlock

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keyPropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.credlock.credlock"
+    namespace = "com.yuklore.credlock"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -31,7 +31,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.credlock.app"
+        applicationId = "com.yuklore.credlock"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
